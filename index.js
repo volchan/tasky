158b58a616a56ebf266f0343f02becd8f6d0283a
stephane de la torre <s.dlt@hotmail.fr>
2018-03-31 19:06:20 +0200

    session 4/39 done

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 
 app.on("ready", () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false
+  });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
