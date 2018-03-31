7c1c9e6003ef1ebfa4e930f6422a0d1d3ca5b21b
stephane de la torre <s.dlt@hotmail.fr>
2018-03-31 19:24:06 +0200

    session 4/42 done

--- index.js
+++ index.js
@@ -18,9 +18,13 @@ app.on("ready", () => {
 
   const iconName = process.platform === "darwin" ? "iconTemplate.png" : "windows-icon@2x.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  
+
   tray = new Tray(iconPath);
   tray.on("click", () => {
-    mainWindow.show();
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
   });
 });
