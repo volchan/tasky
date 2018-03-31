f634a222ea8bb74ba5f754edb3ec375f88a10ef9
stephane de la torre <s.dlt@hotmail.fr>
2018-03-31 19:20:54 +0200

    session 4/41 done

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ const electron = require("electron");
 
 const { app, BrowserWindow, Tray } = electron;
 
-let mainWindow, trayIcon;
+let mainWindow, tray;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({
     height: 500,
     width: 300,
     frame: false,
-    resizable: false
+    resizable: false,
+    show: false
   });
 
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
   const iconName = process.platform === "darwin" ? "iconTemplate.png" : "windows-icon@2x.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  trayIcon = new Tray(iconPath);
+  
+  tray = new Tray(iconPath);
+  tray.on("click", () => {
+    mainWindow.show();
+  });
 });
