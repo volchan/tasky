3c050a90505c1c384df1000786e0321d27802c3c
stephane de la torre <s.dlt@hotmail.fr>
2018-03-31 19:40:06 +0200

    session 4/45 done

--- index.js
+++ index.js
@@ -16,14 +16,25 @@ app.on("ready", () => {
 
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
-  const iconName = process.platform === "darwin" ? "iconTemplate.png" : "windows-icon@2x.png";
+  const osx = process.platform === "darwin";
+
+  const iconName = osx ? "iconTemplate.png" : "windows-icon@2x.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
   tray = new Tray(iconPath);
   tray.on("click", (event, bounds) => {
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: osx ? y + 30 : y - height,
+        height,
+        width
+      });
       mainWindow.show();
     }
   });
